eec0a32df7c3c5a6775b169733d515281f30f190
ktkim4 <ktkim@intellicode.co.kr>
2023-03-09 09:05:26 +0900

    다국어 처리 i18next 설치

---
+++ src/lang/i18n.js
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEn from './translation.en.json';
+import translationKo from './translation.ko.json';
+
+const resource = {
+  en: {
+    translation: translationEn
+  },
+  ko: {
+    translation: translationKo
+  }
+};
+
+i18n.use(initReactI18next).init({
+  resources: resource,
+  // 초기 설정 언어
+  lng: 'ko',
+  fallbackLng: 'ko',
+  debug: false,
+  keySeparator: '.',
+  interpolation: {
+    escapeValue: false
+  }
+});
+export default i18n;
