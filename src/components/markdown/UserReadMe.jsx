8c996acde15f8dc66af54c75a7f89cd7d6de0d6c
ktkim4 <ktkim@intellicode.co.kr>
2023-03-15 11:25:54 +0900

    마크다운 형식으로 작성하면 랜더링 가능, API로 받아오는 값 테스트 필요

--- src/components/markdown/UserReadMe.jsx
+++ src/components/markdown/UserReadMe.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 
 const UserReadMe = () => {
-  const readMkardown = `### Hi there 👋
-
-[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fkkt9102%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)                 
-
-![kkt9102's github stats](https://github-readme-stats.vercel.app/api?username=kkt9102&show_icons=true)
+  // 해당 영역에다 md 문법으로 작성
+  const readMkardown = `
 `
 
   return (
